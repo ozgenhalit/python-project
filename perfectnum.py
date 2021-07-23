240a7463e2fef26a2328ea325f79137addc85c74
stevie <halitozgen@gmail.com>
2021-07-23 03:43:45 +0300

    new feature added

--- perfectnum.py
+++ perfectnum.py
@@ -1,4 +1,4 @@
-num = int(input(Enter a number....:))
+num = int(input("Enter a number....:"))
 if num == 0 :
     num = 1
 elif num < 0 :
@@ -7,9 +7,9 @@ sum_div = 0
 for i in range(1,num+1) :
     if num % i == 0 :
         sum_div += i 
-print(sum_div,= The sum of the divisors of {} including itself.format(num))
-print(sum_div - num,= Its value is the sum of the divisors of {} excluding itself..format(num))
+print("sum_div,= The sum of the divisors of {} including itself".format(num))
+print("sum_div - num,= Its value is the sum of the divisors of {} excluding itself.".format(num))
 if sum_div == num * 2 :
-    print(Perfect Number)
+    print("Perfect Number")
 else :
-    print(Not a Perfect Number)
+    print("Not a Perfect Number")
